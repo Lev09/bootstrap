df1fdaad085f20ef244f08cc57fbad93acc16e6f
Levon <levon.hakopyan@google.com>
2014-06-16 12:15:08 +0400

    create app

---
+++ bootstrap.js
@@ -0,0 +1,45 @@
+var fs = require("fs");
+var mkdirp = require("mkdirp");
+var _ = require('underscore'); 
+
+var argv = require('optimist')
+    .default({
+      pidFolder: "/tmp/process_manager/" 
+    })
+    .argv;
+    
+var processes = [
+	{id: '02', cmd: 'node', params: '/home/levon/git/starter.js'},
+	{id: '03', cmd: 'node', params: '/home/levon/git/process_manager.js'}
+];
+
+var spawn = require("child_process").spawn;
+
+var run = function(process) {
+  
+  var child = spawn(process.cmd, process.params, {detached: true});
+  console.log("Process pid: " + child.pid);
+
+  mkdirp(argv.pidFolder + process.id, function(error) {
+    if(error) console.error(error);
+    else{
+      console.log("folders have been created");
+      fs.writeFile(argv.pidFolder +  process.id + "/" + child.pid, "", function(error) {
+        if (error) console.error(error)
+        else console.log("file created!");
+      });
+    }
+  
+  child.unref();
+  });
+  
+};
+
+_.each(processes, function(process) {
+	run(process);
+});
+
+setTimeout(function() {
+	process.exit(0);
+}, 10000);
+
